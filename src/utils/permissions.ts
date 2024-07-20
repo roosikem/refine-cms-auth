4e70815693e0b41adcfd0af20d963e97838ce098
roosikem <manishprajapati92@gmail.com>
2024-07-20 22:33:29 +0530

    custom sidebar added

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -5,4 +5,12 @@ import { Permission } from "../models/Permission";
 export const hasPermission = (permissions: Permission[], requiredPermission: string): boolean => {
     return permissions.some(p => p.authority === requiredPermission);
 };
+
+function hasAnyPermission(permissions: Permission[], matchPerm:[]) {
+  // Create a map of authorities from the permissions array
+  const permissionAuthorities = permissions.map(permission => permission.authority);
+  
+  // Use some to check if any matchPerm element exists in permissionAuthorities
+  return matchPerm.some(permission => permissionAuthorities.includes(permission));
+}
                 
